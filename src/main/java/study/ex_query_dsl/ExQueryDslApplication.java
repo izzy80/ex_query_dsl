f1574e79e56ca4e45c9b482617b28dfb4c32b671
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-11-26 15:10:41 +0900

    Init:new project start

---
+++ src/main/java/study/ex_query_dsl/ExQueryDslApplication.java
@@ -0,0 +1,13 @@
+package study.ex_query_dsl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExQueryDslApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExQueryDslApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/ex_query_dsl/ExQueryDslApplicationTests.java
@@ -0,0 +1,13 @@
+package study.ex_query_dsl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExQueryDslApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
