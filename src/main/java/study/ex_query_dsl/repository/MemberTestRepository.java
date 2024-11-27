0967a1ea1bff30e119409d0a554fdab30f6e1573
izzy80 <115052929+izzy80@users.noreply.github.com>
2024-11-28 00:07:59 +0900

    Feat:Querydsl 지원 클래스 직접 만들기

---
+++ src/main/java/study/ex_query_dsl/repository/MemberTestRepository.java
@@ -0,0 +1,101 @@
+package study.ex_query_dsl.repository;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.support.PageableExecutionUtils;
+import org.springframework.stereotype.Repository;
+import study.ex_query_dsl.dto.MemberSearchCondition;
+import study.ex_query_dsl.entity.Member;
+import study.ex_query_dsl.repository.support.Querydsl4RepositorySupport;
+
+import static org.springframework.util.StringUtils.hasText;
+import static study.ex_query_dsl.entity.QMember.*;
+import static study.ex_query_dsl.entity.QTeam.team;
+
+import java.util.List;
+
+@Repository
+public class MemberTestRepository extends Querydsl4RepositorySupport {
+
+    public MemberTestRepository() {
+        super(Member.class);
+    }
+
+    public List<Member> basicSelect() {
+        return select(member)
+                .from(member)
+                .fetch();
+    }
+
+    public List<Member> basicSelectFrom() {
+        return selectFrom(member)
+                .fetch();
+    }
+
+    public Page<Member> searchPageByApplyPage(MemberSearchCondition condition, Pageable pageable) {
+        //앞에서 배운 이게 그나마 최적화한 버전
+        JPAQuery<Member> query = selectFrom(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                );
+
+        List<Member> content = getQuerydsl().applyPagination(pageable, query).fetch();
+
+        return PageableExecutionUtils.getPage(content, pageable, query::fetchCount);
+    }
+
+    public Page<Member> applyPagination(MemberSearchCondition condition, Pageable pageable) {
+        //이번에는 Querydsl4RepositorySupport에서 만든 걸 사용
+        //이 코드는 위 코드와 완전히 같은 코드다.
+        return applyPagination(pageable, query -> query
+                .selectFrom(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ));
+    }
+
+    public Page<Member> applyPagination2(MemberSearchCondition condition, Pageable pageable) {
+        return applyPagination(pageable, contentQuery -> contentQuery
+                .selectFrom(member)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                ), countQuery -> countQuery
+                .select(member.id)
+                .from(member)
+                .leftJoin(member.team, team)
+                .where(
+                        usernameEq(condition.getUsername()),
+                        teamNameEq(condition.getTeamName()),
+                        ageGoe(condition.getAgeGoe()),
+                        ageLoe(condition.getAgeLoe())
+                )
+        );
+    }
+
+    private BooleanExpression usernameEq(String username) {
+        return hasText(username) ? member.username.eq(username) : null;
+    }
+
+    private BooleanExpression teamNameEq(String teamName) {
+        return hasText(teamName) ? team.name.eq(teamName) : null;
+    }
+
+    private BooleanExpression ageGoe(Integer ageGoe) {
+        return ageGoe != null ? member.age.goe(ageGoe) : null;
+    }
+
+    private BooleanExpression ageLoe(Integer ageLoe) {
+        return ageLoe != null ? member.age.loe(ageLoe) : null;
+    }
+}
